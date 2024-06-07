9d0d92dd212bd253e460967b9f2b110997109d22
AYUSHIIESTS <165452797+AYUSHIIESTS@users.noreply.github.com>
2024-06-07 10:40:24 +0530

    Add files via upload

---
+++ minicalculator.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    double num1, num2;
+    char operation;
+    double result;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    cout << "Enter the operation (+, -, *, /): ";
+    cin >> operation;
+    switch (operation) 
+    {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation." << endl;
+            break;
+    }
+    return 0;
+}
